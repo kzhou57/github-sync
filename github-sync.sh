7e8273c64fa039561307651cfbd1dbe6f747a916
Kevin Zhou <kevin.zhou@kantar.com>
2024-02-28 23:55:21 -0800

    Avoid dubious ownership error

--- github-sync.sh
+++ github-sync.sh
@@ -29,6 +29,8 @@ echo "BRANCHES=$BRANCH_MAPPING"
 
 git config --unset-all http."https://github.com/".extraheader || :
 
+git config --global --add safe.directory /github/workspace
+
 echo "Resetting origin to: https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY"
 git remote set-url origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY"
 
